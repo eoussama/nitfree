f76a93223c3c493bf628b28bd917096e618c21e2
eoussama <oussama_gtacj@live.fr>
2025-02-01 20:50:11 +0100

    refact: prepare script formatting

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,40 +1,47 @@
 // generate stub index.html files for dev entry
-import { execSync } from 'node:child_process'
-import fs from 'fs-extra'
-import chokidar from 'chokidar'
-import { isDev, log, port, r } from './utils'
+import fs from "fs-extra";
+import { execSync } from "node:child_process";
+
+import chokidar from "chokidar";
+import { isDev, log, port, r } from "./utils";
+
+
 
 /**
  * Stub index.html to use Vite in development
  */
 async function stubIndexHtml() {
-  const views = ['popup', 'sidepanel']
+  const views = ["popup", "sidepanel"];
 
   for (const view of views) {
-    await fs.ensureDir(r(`extension/dist/${view}`))
-    let data = await fs.readFile(r(`src/${view}/index.html`), 'utf-8')
+    await fs.ensureDir(r(`extension/dist/${view}`));
+    let data = await fs.readFile(r(`src/${view}/index.html`), "utf-8");
+
     data = data
-      .replace('"./main.ts"', `"http://localhost:${port}/${view}/main.ts"`)
-      .replace('<div id="app"></div>', '<div id="app">Vite server did not start</div>')
-    await fs.writeFile(r(`extension/dist/${view}/index.html`), data, 'utf-8')
-    log('PRE', `stub ${view}`)
+      .replace("'./main.ts'", `"http://localhost:${port}/${view}/main.ts"`)
+      .replace("<div id='app'></div>", "<div id='app'>Vite server did not start</div>");
+
+    await fs.writeFile(r(`extension/dist/${view}/index.html`), data, "utf-8");
+    log("PRE", `stub ${view}`);
   }
 }
 
 function writeManifest() {
-  execSync('npx esno ./scripts/manifest.ts', { stdio: 'inherit' })
+  execSync('npx esno ./scripts/manifest.ts', { stdio: 'inherit' });
 }
 
-writeManifest()
+writeManifest();
 
 if (isDev) {
-  stubIndexHtml()
+  stubIndexHtml();
+
   chokidar.watch(r('src/**/*.html'))
     .on('change', () => {
-      stubIndexHtml()
-    })
+      stubIndexHtml();
+    });
+
   chokidar.watch([r('src/manifest.ts'), r('package.json')])
     .on('change', () => {
-      writeManifest()
-    })
+      writeManifest();
+    });
 }
