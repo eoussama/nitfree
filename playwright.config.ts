1022dc6a981eae0d72704f6a320b76ed6c62720b
eoussama <oussama_gtacj@live.fr>
2025-02-01 20:14:30 +0100

    refact: remove antfu eslint config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,17 @@
 /**
  * @see {@link https://playwright.dev/docs/chrome-extensions Chrome extensions | Playwright}
  */
-import { defineConfig } from '@playwright/test'
+import { defineConfig } from "@playwright/test";
+
+
 
 export default defineConfig({
-  testDir: './e2e',
   retries: 2,
+  testDir: "./e2e",
   webServer: {
-    command: 'npm run dev',
+    command: "npm run dev",
     // start e2e test after the Vite server is fully prepared
-    url: 'http://localhost:3303/popup/main.ts',
+    url: "http://localhost:3303/popup/main.ts",
     reuseExistingServer: true,
-  },
-})
+  }
+});
