f59ba15c3c7d79b34f0aeb017c870a6ff6fd289e
eoussama <oussama_gtacj@live.fr>
2025-02-01 19:50:10 +0100

    feat: database helper, #8

---
+++ src/core/helpers/db.helper.ts
@@ -0,0 +1,16 @@
+import { openDB } from 'idb'
+
+import { CONFIG } from '../consts'
+import type { IDatabase } from '../interfaces'
+
+export class DBHelper {
+  static async init(): Promise<void> {
+    await openDB<IDatabase>(
+      CONFIG.database.name,
+      CONFIG.database.version,
+      {
+
+      },
+    )
+  }
+}
