c77f22619273003cdbf53c62b86e628af3640223
eoussama <oussama_gtacj@live.fr>
2025-02-01 20:58:48 +0100

    refact: manifest formatting, #8

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,10 +1,13 @@
-import fs from 'fs-extra'
-import type { Manifest } from 'webextension-polyfill'
-import type PkgType from '../package.json'
-import { isDev, isFirefox, port, r } from '../scripts/utils'
+import fs from "fs-extra";
+import type { Manifest } from "webextension-polyfill";
+
+import type PkgType from "../package.json";
+import { isDev, isFirefox, port, r } from "../scripts/utils";
+
+
 
 export async function getManifest() {
-  const pkg = await fs.readJSON(r('package.json')) as typeof PkgType
+  const pkg = await fs.readJSON(r("package.json")) as typeof PkgType;
 
   // update this file to update this manifest.json
   // can also be conditional based on your need
@@ -14,65 +17,64 @@ export async function getManifest() {
     version: pkg.version,
     description: pkg.description,
     action: {
-      default_icon: './assets/icon512.png',
-      default_popup: './dist/popup/index.html',
+      default_icon: "./assets/icon512.png",
+      default_popup: "./dist/popup/index.html"
     },
     background: isFirefox
       ? {
-          scripts: ['dist/background/index.mjs'],
-          type: 'module',
+          scripts: ["dist/background/index.mjs"],
+          type: "module"
         }
       : {
-          service_worker: './dist/background/index.mjs',
+          service_worker: "./dist/background/index.mjs"
         },
     icons: {
-      16: './assets/icon16.png',
-      32: './assets/icon32.png',
-      48: './assets/icon48.png',
-      128: './assets/icon128.png',
-      512: './assets/icon512.png',
+      16: "./assets/icon16.png",
+      32: "./assets/icon32.png",
+      48: "./assets/icon48.png",
+      128: "./assets/icon128.png",
+      512: "./assets/icon512.png"
     },
     permissions: [
-      'tabs',
-      'storage',
-      'activeTab',
+      "tabs",
+      "storage",
+      "activeTab"
     ],
-    host_permissions: ['*://discord.com/*'],
+    host_permissions: ["*://discord.com/*"],
     content_scripts: [
       {
         matches: [
-          '*://discord.com/*',
+          "*://discord.com/*"
         ],
         js: [
-          'dist/contentScripts/index.global.js',
-        ],
-      },
+          "dist/contentScripts/index.global.js"
+        ]
+      }
     ],
     web_accessible_resources: [
       {
-        resources: ['dist/contentScripts/style.css'],
-        matches: ['<all_urls>'],
-      },
+        resources: ["dist/contentScripts/style.css"],
+        matches: ["<all_urls>"]
+      }
     ],
     content_security_policy: {
       extension_pages: isDev
         // this is required on dev for Vite script to load
-        ? `script-src \'self\' http://localhost:${port}; object-src \'self\'`
-        : 'script-src \'self\'; object-src \'self\'',
-    },
-  }
+        ? `script-src \"self\" http://localhost:${port}; object-src \"self\"`
+        : "script-src \"self\"; object-src \"self\""
+    }
+  };
 
   // add sidepanel
   if (isFirefox) {
     manifest.sidebar_action = {
-      default_panel: 'dist/sidepanel/index.html',
-    }
-  }
-  else {
+      default_panel: "dist/sidepanel/index.html"
+    };
+  } else {
     // the sidebar_action does not work for chromium based
     (manifest as any).side_panel = {
-      default_path: 'dist/sidepanel/index.html',
-    }
+      default_path: "dist/sidepanel/index.html"
+    };
   }
 
   // FIXME: not work in MV3
@@ -80,9 +82,9 @@ export async function getManifest() {
     // for content script, as browsers will cache them for each reload,
     // we use a background script to always inject the latest version
     // see src/background/contentScriptHMR.ts
-    delete manifest.content_scripts
-    manifest.permissions?.push('webNavigation')
+    delete manifest.content_scripts;
+    manifest.permissions?.push("webNavigation");
   }
 
-  return manifest
+  return manifest;
 }
