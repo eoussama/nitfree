f4b697adcf92a4e6bb7c33587a1f086f96686ae9
eoussama <oussama_gtacj@live.fr>
2025-02-01 20:48:43 +0100

    refact: utility log core dependency, #8

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,12 +1,16 @@
-import { resolve } from 'node:path'
-import process from 'node:process'
-import { bgCyan, black } from 'kolorist'
+import { resolve } from "node:path";
+import process from "node:process";
 
-export const port = Number(process.env.PORT || '') || 3303
-export const r = (...args: string[]) => resolve(__dirname, '..', ...args)
-export const isDev = process.env.NODE_ENV !== 'production'
-export const isFirefox = process.env.EXTENSION === 'firefox'
+import { bgCyan, black } from "kolorist";
+import { LogHelper } from "../src/core";
+
+
+
+export const port = Number(process.env.PORT || "") || 3303;
+export const r = (...args: string[]) => resolve(__dirname, "..", ...args);
+export const isDev = process.env.NODE_ENV !== "production";
+export const isFirefox = process.env.EXTENSION === "firefox";
 
 export function log(name: string, message: string) {
-  console.log(black(bgCyan(` ${name} `)), message)
+  LogHelper.print(black(bgCyan(` ${name} `)), message);
 }
