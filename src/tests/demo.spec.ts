dba1594e56e06f83f92f1712ec92e90542445381
eoussama <oussama_gtacj@live.fr>
2025-02-01 21:00:11 +0100

    refact: demo spec alignment, #8

--- src/tests/demo.spec.ts
+++ src/tests/demo.spec.ts
@@ -1,7 +1,9 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from "vitest";
 
-describe('demo', () => {
-  it('should work', () => {
-    expect(1 + 1).toBe(2)
-  })
-})
+
+
+describe("demo", () => {
+  it("should work", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
