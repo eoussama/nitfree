08ed41703343fbfce40de34cce3ec34a90f239a0
eoussama <oussama_gtacj@live.fr>
2025-02-05 00:11:47 +0100

    test: basic logo test

--- src/components/__tests__/Logo.test.ts
+++ src/components/__tests__/Logo.test.ts
@@ -1,11 +1,13 @@
-import { describe, expect, it } from 'vitest'
-import { mount } from '@vue/test-utils'
-import Logo from '../Logo.vue'
+import { describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
+import Logo from "../Logo.vue";
 
-describe('logo component', () => {
-  it('should render', () => {
-    const wrapper = mount(Logo)
 
-    expect(wrapper.html()).toBeTruthy()
-  })
-})
+
+describe("logo component", () => {
+  it("should render", () => {
+    const wrapper = mount(Logo);
+
+    expect(wrapper.html()).toBeTruthy();
+  });
+});
