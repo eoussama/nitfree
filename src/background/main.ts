c984cb0ea852f7e708e02abed6a63c9c9c4fb425
eoussama <oussama_gtacj@live.fr>
2025-02-07 00:21:53 +0100

    feat: wrapped indexedDB, closes #8

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,5 +1,4 @@
-import { onMessage, sendMessage } from "webext-bridge/background";
-import type { Tabs } from "webextension-polyfill";
+import { LogHelper } from "~/core";
 
 
 
@@ -12,46 +11,5 @@ if (import.meta.hot) {
 }
 
 browser.runtime.onInstalled.addListener((): void => {
-  // eslint-disable-next-line no-console
-  console.log("Extension installed");
-});
-
-let previousTabId = 0;
-
-// communication example: send previous tab title from background page
-// see shim.d.ts for type declaration
-browser.tabs.onActivated.addListener(async ({ tabId }) => {
-  if (!previousTabId) {
-    previousTabId = tabId;
-    return;
-  }
-
-  let tab: Tabs.Tab;
-
-  try {
-    tab = await browser.tabs.get(previousTabId);
-    previousTabId = tabId;
-  }
-  catch {
-    return;
-  }
-
-  // eslint-disable-next-line no-console
-  console.log("previous tab", tab);
-  sendMessage("tab-prev", { title: tab.title }, { context: "content-script", tabId });
-});
-
-onMessage("get-current-tab", async () => {
-  try {
-    const tab = await browser.tabs.get(previousTabId);
-
-    return {
-      title: tab?.title
-    }
-  }
-  catch {
-    return {
-      title: undefined
-    }
-  }
+  LogHelper.print("Nitfree was installed successfully!");
 });
