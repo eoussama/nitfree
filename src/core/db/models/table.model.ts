352e761510407b8db56fa8ad1f415fd3726b9684
eoussama <oussama_gtacj@live.fr>
2025-02-09 02:03:22 +0100

    feat: generic table model, #9

---
+++ src/core/db/models/table.model.ts
@@ -0,0 +1,47 @@
+import { ETable } from "../enums";
+import { EnumHelper, TNullable } from "../../common";
+
+
+
+export class Table {
+
+  private static instance: Table;
+
+  protected readonly name: ETable;
+  protected readonly db: IDBDatabase;
+
+  private get tableName(): string {
+    return <string>EnumHelper.toString(ETable, this.name);
+  }
+
+  protected get table(): IDBTransaction {
+    return this.db.transaction(this.tableName, "readwrite");
+  }
+
+  protected constructor(db: IDBDatabase, name: ETable) {
+    this.db = db;
+    this.name = name;
+
+    Table.instance = this;
+  }
+
+  protected init(): TNullable<IDBObjectStore> {
+    if (this.exists()) {
+      return null;
+    }
+
+    return this.db.createObjectStore(this.tableName, { keyPath: "id" });
+  }
+  
+  protected exists(): boolean {
+    return this.db.objectStoreNames.contains(this.tableName);
+  }
+
+  public static init(db: IDBDatabase, name: ETable): TNullable<IDBObjectStore> {
+    return new Table(db, name).init();
+  }
+
+  public static getInstance(): Table {
+    return this.instance;
+  }
+}
