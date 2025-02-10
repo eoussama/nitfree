8e3fc2a8013d1abcc17e8f7677d1eadcb42e70ce
eoussama <oussama_gtacj@live.fr>
2025-02-10 01:13:13 +0100

    feat: improved logger, #1

--- src/core/common/helpers/log.helper.ts
+++ src/core/common/helpers/log.helper.ts
@@ -1,14 +1,22 @@
+import { CONFIG } from "../consts";
 import { TUnsafe, TPrimitive } from "../types";
 
 
 
 export class LogHelper {
   
-  static print(...args: Array<TUnsafe<TPrimitive>>) {
-    console.info("[Nitfree]", ...args);
+  public static print(...args: Array<TUnsafe<TPrimitive>>) {
+    console.info(...this.format(...args));
   }
 
-  static error(...args: Array<TUnsafe<TPrimitive>>) {
-    console.error("[Nitfree]", ...args);
+  public static error(...args: Array<TUnsafe<TPrimitive>>) {
+    console.error(...this.format(...args));
+  }
+
+  private static format(...args: Array<TUnsafe<TPrimitive>>): Array<TUnsafe<TPrimitive>> {
+    const name = CONFIG.name.toUpperCase();
+    const prefix = `[${name}]`;
+
+    return [prefix, ...args];
   }
 }
