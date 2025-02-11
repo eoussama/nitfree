0c15565ee52d9231175e5ca75513ade99cf57742
eoussama <oussama_gtacj@live.fr>
2025-02-11 13:55:09 +0100

    feat: metadata table adaptation, #15

--- src/core/db/models/metadata.model.ts
+++ src/core/db/models/metadata.model.ts
@@ -1,21 +1,18 @@
 import { ETable } from "../enums";
 import { Table } from "./table.model";
-import { TNullable } from "../../common";
 
 
 
 export class Metadata extends Table {
 
-  public static init(db: IDBDatabase): TNullable<IDBObjectStore> {
-    const table = super.init(db, ETable.Metadata);
-    
-    if (table) {
-      table.createIndex("titleIdx", "title", { unique: false });
-      table.createIndex("tagIdIdx", "tagId", { unique: true });
-      table.createIndex("fileIdIdx", "fileId", { unique: false });
-      table.createIndex("favoriteIdx", "favorite", { unique: false });
-    }
+  public constructor(db: IDBDatabase) {
+    super(db, ETable.Metadata);
+  }
 
-    return table;
+  protected override onInit(table: IDBObjectStore): void {
+    table.createIndex("titleIdx", "title", { unique: false });
+    table.createIndex("tagIdIdx", "tagId", { unique: true });
+    table.createIndex("fileIdIdx", "fileId", { unique: false });
+    table.createIndex("favoriteIdx", "favorite", { unique: false });
   }
 }
