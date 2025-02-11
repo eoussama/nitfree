4f124743a9b08a195188823a35f9dde03e7e8a18
eoussama <oussama_gtacj@live.fr>
2025-02-11 13:54:42 +0100

    feat: tags table adaptation, #14

--- src/core/db/models/tags.model.ts
+++ src/core/db/models/tags.model.ts
@@ -1,18 +1,14 @@
 import { ETable } from "../enums";
 import { Table } from "./table.model";
-import { TNullable } from "../../common";
 
 
 
 export class Tags extends Table {
+  public constructor(db: IDBDatabase) {
+    super(db, ETable.Tags);
+  }
 
-  public static init(db: IDBDatabase): TNullable<IDBObjectStore> {
-    const table = super.init(db, ETable.Tags);
-    
-    if (table) {
-      table.createIndex("nameIdx", "name", { unique: false });
-    }
-
-    return table;
+  protected override onInit(table: IDBObjectStore): void {
+    table.createIndex("nameIdx", "name", { unique: false });
   }
 }
