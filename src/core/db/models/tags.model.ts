9bc9f6b527b48ef6bc0e241c10a2b22d2d8312e6
eoussama <oussama_gtacj@live.fr>
2025-02-11 14:35:09 +0100

    feat: tags creation, #16

--- src/core/db/models/tags.model.ts
+++ src/core/db/models/tags.model.ts
@@ -1,14 +1,38 @@
 import { ETable } from "../enums";
 import { Table } from "./table.model";
+import { ITable, ITag } from "../interfaces";
 
 
 
-export class Tags extends Table {
+export class Tags extends Table implements ITable<ITag> {
   public constructor(db: IDBDatabase) {
     super(db, ETable.Tags);
   }
 
   protected override onInit(table: IDBObjectStore): void {
     table.createIndex("nameIdx", "name", { unique: false });
+  }
+
+  create(model: Omit<ITag, "id" | "createdOn" | "updatedOn">): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const createdOn = new Date();
+      const updatedOn = new Date();
+      const id = crypto.randomUUID();
+
+      const value: ITag = {
+        id, createdOn, updatedOn,
+        ...model
+      };
+
+      const request = this.table.add(value);
+
+      request.onsuccess = () => {
+        resolve(id);
+      };
+
+      request.onerror = () => {
+        reject(new Error("Could not insert tag"));
+      };
+    });
   }
 }
