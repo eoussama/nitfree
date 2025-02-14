11358206f7cf8ea895a0bb9d35f3392376c4538d
eoussama <oussama_gtacj@live.fr>
2025-02-14 20:37:11 +0100

    refact: inherited creation method, #13

--- src/core/db/models/files.model.ts
+++ src/core/db/models/files.model.ts
@@ -1,9 +1,10 @@
 import { ETable } from "../enums";
+import { IFile } from "../interfaces";
 import { Table } from "./table.model";
 
 
 
-export class Files extends Table {
+export class Files extends Table<IFile> {
 
   public constructor(db: IDBDatabase) {
     super(db, ETable.Files);
