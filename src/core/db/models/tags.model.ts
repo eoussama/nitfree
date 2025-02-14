7435d4774f267f8d5884c9cd8fffa19aacae7dbe
eoussama <oussama_gtacj@live.fr>
2025-02-14 20:37:02 +0100

    refact: inherited creation method, #14

--- src/core/db/models/tags.model.ts
+++ src/core/db/models/tags.model.ts
@@ -1,32 +1,16 @@
 import { ETable } from "../enums";
+import { ITag } from "../interfaces";
 import { Table } from "./table.model";
-import { ITable, ITag } from "../interfaces";
-import { TStrip } from "../types";
-import { BaseHelper } from "../helpers";
 
 
 
-export class Tags extends Table implements ITable<ITag> {
+export class Tags extends Table<ITag> {
+
   public constructor(db: IDBDatabase) {
     super(db, ETable.Tags);
   }
 
   protected override onInit(table: IDBObjectStore): void {
     table.createIndex("nameIdx", "name", { unique: false });
-  }
-
-  create(tag: TStrip<ITag>): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const value = BaseHelper.create(tag);
-      const request = this.table.add(value);
-
-      request.onsuccess = () => {
-        resolve(value.id);
-      };
-
-      request.onerror = () => {
-        reject(new Error("Could not insert tag"));
-      };
-    });
   }
 }
