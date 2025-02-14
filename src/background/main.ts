9b67d94611114795e33ed71f21db5fb8b6efdc9f
eoussama <oussama_gtacj@live.fr>
2025-02-15 00:40:13 +0100

    refact: cleared main script, #8

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,4 +1,4 @@
-import { CONFIG, Database, LogHelper, StringHelper } from "~/core";
+import { CONFIG, LogHelper, StringHelper } from "~/core";
 
 
 
@@ -14,44 +14,7 @@ browser.runtime.onInstalled.addListener(async (): Promise<void> => {
   LogHelper.print(`${name} was installed successfully!`);
 
   try {
-    const db = await Database.getInstance();
-
-    const tag = await db.TAGS.create({
-      name: "tag" + Math.random() * 1000,
-      description: "my description here !1!!"
-    });
-
-    const file = await db.FILES.create({
-      data: new Blob(["pass=123"], { type: "text/plain" })
-    });
-
-    const data = await db.METADATA.create({
-      description: "this is a text file *",
-      fileId: file.id,
-      favorite: true,
-      title: "Secret Sauce",
-      tagId: tag.id
-    });
-
-    LogHelper.print("created object with id =", data.id);
-
-    const fetchedData = await db.FILES.get(file.id);
-
-    LogHelper.print("the created file", file.id, "is", fetchedData.data.size, "bytes");
-
-    const updatedFile = await db.FILES.update(file.id, {
-      data: new Blob(["password=dea//7frez_feEE/7"], { type: "text/plain" })
-    });
-
-    LogHelper.print("the updated file", updatedFile.id, "is", updatedFile.data.size, "bytes");
-    
-    await db.FILES.delete(updatedFile.id);
-
-    LogHelper.print("the file", updatedFile.id, "was deleted");
-
-    for (const t of await db.TAGS.getAll()) {
-      LogHelper.print("tag =>", t.name);
-    }
+    // TODO: init
   } catch(err) {
     LogHelper.error((err as unknown as Error).message);
   }
