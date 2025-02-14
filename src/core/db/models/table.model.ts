a9452c61cc5521fa6ec94804d8f8e2b04a7741bb
eoussama <oussama_gtacj@live.fr>
2025-02-14 21:23:32 +0100

    feat: entiry update, resolves #18

--- src/core/db/models/table.model.ts
+++ src/core/db/models/table.model.ts
@@ -61,18 +61,35 @@ export class Table<T extends IBase> {
     });
   }
   
-  public create(obj: TStrip<T>): Promise<IBase["id"]> {
+  public create(obj: TStrip<T>): Promise<T> {
     return new Promise((resolve, reject) => {
       const value = BaseHelper.create(obj);
       const request = this.table.add(value);
 
       request.onsuccess = () => {
-        resolve(value.id);
+        resolve(value);
       };
 
       request.onerror = () => {
         reject(new Error("Could not insert object"));
       };
+    });
+  }
+
+  public update(id: IBase["id"], obj: Partial<TStrip<T>>): Promise<T> {
+    return new Promise((resolve, reject) => {
+      this.get(id).then(oldValue => {
+        const newValue = BaseHelper.update(oldValue, obj);
+        const request = this.table.put(newValue);
+
+        request.onsuccess = () => {
+          resolve(newValue);
+        };
+
+        request.onerror = () => {
+          reject(new Error("Could not update object"));
+        };
+      });
     });
   }
  }
