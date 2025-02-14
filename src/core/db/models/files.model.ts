b917e8cbac56c86328c03af3c18d3312e83d4376
eoussama <oussama_gtacj@live.fr>
2025-02-14 20:43:04 +0100

    refact: removed file indexes, #13

--- src/core/db/models/files.model.ts
+++ src/core/db/models/files.model.ts
@@ -9,9 +9,4 @@ export class Files extends Table<IFile> {
   public constructor(db: IDBDatabase) {
     super(db, ETable.Files);
   }
-
-  protected override onInit(table: IDBObjectStore): void {    
-    table.createIndex("nameIdx", "name", { unique: false });
-    table.createIndex("typeIdx", "type", { unique: false }); 
-  }
 }
