0d0963e61963a93098a5f990d41ea11205bc2622
eoussama <oussama_gtacj@live.fr>
2025-02-17 00:17:53 +0100

    refact: content script cleanup

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,34 +1,21 @@
 import { createApp } from "vue";
-import { onMessage } from "webext-bridge/content-script";
-
-import { LogHelper, setupApp } from "~/core";
 
 import App from "./views/App.vue";
+import { setupApp } from "~/core";
 
 
 
-// Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
   if (document.getElementById(__NAME__)) {
-    LogHelper.print("[Nitfree] Container already exists, skipping injection.");
     return;
   }
 
-  LogHelper.print("[Nitfree] Hello world from content script!");
-
-  // communication example: send previous tab title from background page
-  onMessage("tab-prev", ({ data }) => {
-    LogHelper.print(`[Nitfree] Navigate from page "${data.title}"`);
-  });
-
-  // mount component to context window
-  const container = document.createElement("div");
-  container.id = __NAME__;
-
   const root = document.createElement("div");
   const styleEl = document.createElement("link");
+  const container = document.createElement("div");
   const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? "open" : "closed" }) || container;
 
+  container.id = __NAME__;
   styleEl.setAttribute("rel", "stylesheet");
   styleEl.setAttribute("href", browser.runtime.getURL("dist/contentScripts/style.css"));
 
@@ -36,8 +23,6 @@ import App from "./views/App.vue";
   shadowDOM.appendChild(root);
   
   setTimeout(() => {
-    LogHelper.print("[Nitfree] Inject now");
-
     const buttons = document.querySelector("[class^=\"channelBottomBarArea_\"] [class^=\"channelTextArea_\"] [class^=\"buttons_\"]");
     if (buttons) {
       buttons.appendChild(container);
