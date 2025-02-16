7d1d836689693dc748c4f511cf8c474beb53777b
eoussama <oussama_gtacj@live.fr>
2025-02-17 00:31:48 +0100

    refact: cleaned up the content script entry file

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,7 +1,5 @@
-import { createApp } from "vue";
-
+import { inject } from "~/core";
 import App from "./views/App.vue";
-import { setupApp } from "~/core";
 
 
 
@@ -10,27 +8,5 @@ import { setupApp } from "~/core";
     return;
   }
 
-  const root = document.createElement("div");
-  const styleEl = document.createElement("link");
-  const container = document.createElement("div");
-  const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? "open" : "closed" }) || container;
-
-  container.id = __NAME__;
-  styleEl.setAttribute("rel", "stylesheet");
-  styleEl.setAttribute("href", browser.runtime.getURL("dist/contentScripts/style.css"));
-
-  shadowDOM.appendChild(styleEl);
-  shadowDOM.appendChild(root);
-  
-  setTimeout(() => {
-    const buttons = document.querySelector("[class^=\"channelBottomBarArea_\"] [class^=\"channelTextArea_\"] [class^=\"buttons_\"]");
-    if (buttons) {
-      buttons.appendChild(container);
-    }
-
-    const app = createApp(App);
-
-    setupApp(app);
-    app.mount(root);
-  }, 3500);
+  inject(App, "[class^=\"channelBottomBarArea_\"] [class^=\"channelTextArea_\"] [class^=\"buttons_\"]", __NAME__);
 })();
