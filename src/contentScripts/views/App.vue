ac70b6d1aa8af0e016f24143dcabe4ffc96d1187
eoussama <oussama_gtacj@live.fr>
2025-03-01 22:49:09 +0000

    refact: cleanup, #24

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
-import './../../styles/nitfree-trigger.scss'
+import { LogHelper } from "~/core";
+import "./../../styles/nitfree-trigger.scss";
 
-let opened: boolean = false
+
+
+let opened: boolean = false;
 
 function onclick() {
+  LogHelper.print({ opened });
+
   if (opened) {
-    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'escape' }))
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'escape' }));
   }
   else {
-    const btn = document.getElementById('nitfree')?.previousSibling?.childNodes[0] as HTMLButtonElement
+    const btn = document.getElementById('nitfree')?.previousSibling?.childNodes[0] as HTMLButtonElement;
 
     if (btn) {
-      btn.click()
+      btn.click();
     }
   }
 
-  opened = !opened
+  opened = !opened;
 }
 </script>
 
