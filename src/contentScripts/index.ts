f9c56c0ece5206d13151514f0fdb9717fbf318ac
eoussama <oussama_gtacj@live.fr>
2025-03-01 22:09:50 +0000

    refact: removed check from entry file, #23

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -4,9 +4,5 @@ import App from "./views/App.vue";
 
 
 (() => {
-  if (document.getElementById(__NAME__)) {
-    return;
-  }
-
   inject(App, "[class^=\"channelBottomBarArea_\"] [class^=\"channelTextArea_\"] [class^=\"buttons_\"]", __NAME__);
 })();
