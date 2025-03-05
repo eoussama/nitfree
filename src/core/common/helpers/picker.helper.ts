7743791e986a4646c1ce474f201f6b2c3d5409b0
eoussama <oussama_gtacj@live.fr>
2025-03-05 17:15:44 +0000

    feat: picker trigger, #24

--- src/core/common/helpers/picker.helper.ts
+++ src/core/common/helpers/picker.helper.ts
@@ -1,5 +1,16 @@
+declare const __NAME__: string;
+
 export class PickerHelper {
   static isOpen(): boolean {
     return document.getElementById("[class*=\"expressionPickerPositionLayer__\"]") !== null;
   } 
+
+  static toggle(): void {
+    const root = document.getElementById(__NAME__);
+    const btn = root?.previousSibling?.childNodes[0] as HTMLButtonElement;
+
+    if (btn) {
+      btn.click();
+    }
+  }
 }
